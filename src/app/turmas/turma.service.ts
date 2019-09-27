cee51e1cbef201a8517220b03875186d3f5d949a
MateusO97 <mat97_@hotmail.com>
2019-09-27 19:58:36 -0300

    Ajustando rota para deletar turma
    Co-authored-by: Gabrielle-Ribeiro <gabrielleribeiro2010@gmail.com>
    Co-authored-by: GustavoAPS <gugustavo137@gmail.com>
    Co-authored-by: ArthurPaivaT <arthur001123@gmail.com>

--- src/app/turmas/turma.service.ts
+++ src/app/turmas/turma.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Turma } from './turmas.model';
 import { HttpClient } from '@angular/common/http';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +11,10 @@ export class TurmaService {
   constructor(private http: HttpClient) { }
 
   public get_turmas(): Observable<any> {
-    return this.http.get('http://0.0.0.0:8000/turmas');
+    return this.http.get('http://0.0.0.0:8000/turmas/');
+  }
+
+  public delete_turmas(id): Observable<any>{
+    return this.http.delete('http://0.0.0.0:8000/turma_delete/'+id, id);
   }
 }
-
