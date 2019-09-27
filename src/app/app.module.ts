82fdb120a395ff0fc05f5bd484e6b128935528be
Danilow200 <danilow200@gmail.com>
2019-09-27 15:30:39 -0300

    Inicio da lista de matricula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlunoAutoRegistroComponent } from './aluno-auto-registro/aluno-auto-registro.component';
+import { ListaDeMatriculaComponent } from './lista-de-matricula/lista-de-matricula.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunoAutoRegistroComponent
+    AlunoAutoRegistroComponent,
+    ListaDeMatriculaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lista-de-matricula/lista-de-matricula.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-matricula',
+  templateUrl: './lista-de-matricula.component.html',
+  styleUrls: ['./lista-de-matricula.component.css']
+})
+export class ListaDeMatriculaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
