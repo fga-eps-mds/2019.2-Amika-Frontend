baee254d163f30a36dd872bf3637b9dd4a86de9e
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-09-27 17:18:25 -0300

    Consertando o forms e a estilização do botão Signed-off-by: robertomatheus <matheus456456@gmail.com>

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,6 +8,7 @@ import { Router } from "@angular/router";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   formularioLogin: FormGroup;
 
