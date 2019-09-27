9545d3ba15e15dc45a6701be5a04f2900e2c0e75
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-27 17:04:05 -0300

    Criaçao das funçoes para submeter o form e atribuir os valores de input html ao objeto

--- src/app/aluno-auto-registro/aluno-auto-registro.component.ts
+++ src/app/aluno-auto-registro/aluno-auto-registro.component.ts
@@ -1,15 +1,42 @@
+import { Matricula } from './../matriculas/shared/matricula';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormControlDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-aluno-auto-registro',
   templateUrl: './aluno-auto-registro.component.html',
-  styleUrls: ['./aluno-auto-registro.component.css']
+  styleUrls: ['./aluno-auto-registro.component.css'],
 })
 export class AlunoAutoRegistroComponent implements OnInit {
 
+  formulario: FormGroup;
+
   constructor() { }
 
+  // Esse evento é disparado sempre que o componente é inicializado
   ngOnInit() {
+    /*this.formulario = new FormGroup({
+      matricula: new FormControl(null),
+      nome: new FormControl(null),
+      email: new FormControl(null),
+      senha: new FormControl(null)
+    });*/
+
+    this.createForm(new Matricula());
+  }
+
+  OnSubmit() {
+    console.log(this.formulario);
+    this.formulario.reset(new Matricula());
+  }
+
+  createForm(matricula: Matricula) {
+    this.formulario = new FormGroup({
+      matricula: new FormControl(matricula.matricula),
+      nome: new FormControl(matricula.nome),
+      email: new FormControl(matricula.email),
+      senha: new FormControl(matricula.senha)
+    });
   }
 
 }
