a076876e3272e63bcc1a1ec2831d96575e0929cc
Maicon Mares <maiconmaresunb@gmail.com>
2019-09-30 17:34:25 -0300

    Tratamento do formulario e cadastro do mesmo no banco por meio do service de requisiçao

--- src/app/aluno-auto-registro/aluno-auto-registro.component.ts
+++ src/app/aluno-auto-registro/aluno-auto-registro.component.ts
@@ -1,6 +1,9 @@
 import { Matricula } from './../matriculas/shared/matricula';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormControlDirective } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { RequisicaoService } from '../requisicao.service';
 
 @Component({
   selector: 'app-aluno-auto-registro',
@@ -11,32 +14,25 @@ export class AlunoAutoRegistroComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router:Router, private requisicaoService:RequisicaoService) {
+    this.formulario = this.formBuilder.group({
+      matricula: ['', Validators.required],
+      nome: ['', Validators.required],
+      email: ['', Validators.required],
+      senha: ['', Validators.required]
+    })
+  }
 
-  // Esse evento é disparado sempre que o componente é inicializado
+  cadastrarAluno(dadosAluno) {
+    console.log('Usuario cadastrado!');
+    let dados = {"matricula" : dadosAluno.matricula, "nome": dadosAluno.nome, "email": dadosAluno.email, "senha": dadosAluno.senha}
+    console.log(dados)
+    this.requisicaoService.realizarRequisicao(dados);
+    console.log(this.requisicaoService.errors);
+  }
+
   ngOnInit() {
-    /*this.formulario = new FormGroup({
-      matricula: new FormControl(null),
-      nome: new FormControl(null),
-      email: new FormControl(null),
-      senha: new FormControl(null)
-    });*/
 
-    this.createForm(new Matricula());
-  }
-
-  OnSubmit() {
-    console.log(this.formulario);
-    this.formulario.reset(new Matricula());
-  }
-
-  createForm(matricula: Matricula) {
-    this.formulario = new FormGroup({
-      matricula: new FormControl(matricula.matricula),
-      nome: new FormControl(matricula.nome),
-      email: new FormControl(matricula.email),
-      senha: new FormControl(matricula.senha)
-    });
   }
 
 }
