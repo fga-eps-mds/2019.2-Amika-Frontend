8f50bdf928adade0d475caa42b82cdb08630f9cb
MateusO97 <mat97_@hotmail.com>
2019-10-01 09:53:15 -0300

    Utilizando variavel de ambiente para passar url

--- src/app/turmas/turma.service.ts
+++ src/app/turmas/turma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -10,22 +11,22 @@ export class TurmaService {
   constructor(private http: HttpClient) { }
 
   public get_turmas(): Observable<any> {
-    return this.http.get('http://0.0.0.0:8000/turmas/');
+    return this.http.get(environment.urlApi +'turmas/');
   }
 
   public delete_turmas(id): Observable<any>{
-    return this.http.delete('http://0.0.0.0:8000/turma_delete/'+id, id);
+    return this.http.delete(environment.urlApi +'turma_delete/'+id, id);
   }
 
   public create_turmas(turma): Observable<any> {
-    return this.http.post('http://0.0.0.0:8000/turmas_create', turma);
+    return this.http.post(environment.urlApi +'turmas_create', turma);
   }
 
   public edit_turmas(id, turma): Observable<any> {
-    return this.http.put('http://0.0.0.0:8000/turma_edit/'+id, turma);
+    return this.http.put(environment.urlApi +'turma_edit/'+id, turma);
   }
 
   public getById(id) {
-    return this.http.get('http://0.0.0.0:8000/turma/'+id, id);
+    return this.http.get(environment.urlApi +'turma/'+id, id);
   }
 }
