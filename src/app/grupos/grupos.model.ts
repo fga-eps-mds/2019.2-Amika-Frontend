50bc87c8f8410933db9035544acdd95a64ae3b5c
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-10-12 19:56:16 -0300

    Adicionando exemplos na model

--- src/app/grupos/grupos.model.ts
+++ src/app/grupos/grupos.model.ts
@@ -1,4 +1,24 @@
 export class Grupo {
     public id: number;
-    public nomeGrupo: string;
+    public nome: string;
 }
+
+export interface Grupo {
+    id: number; 
+    nome: string;
+  }
+
+  let grupo: Grupo[] = [
+    {
+      id: 56,
+      nome: 'Grupo 1'
+    },
+    {
+      id: 175,
+      nome: 'Grupo 2'
+    },
+    {
+        id: 12,
+        nome: 'Grupo 3'
+    }
+  ];
