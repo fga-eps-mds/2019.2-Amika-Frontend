edd3ae84a02c8fcc1dfa26760708f521011ab491
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-10-12 15:38:50 -0300

    Criação dos métodos get e delete dos grupos

--- src/app/grupo.service.ts
+++ src/app/grupo.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GrupoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public get_grupos(): Observable<any> {
+    return this.http.get(environment.urlApi + 'grupo/')
+  }
+
+  public delete_grupos(id): Observable<any> {
+    return this.http.delete(environment.urlApi + 'grupo/' + id, id);
+  }
+
+
 }
