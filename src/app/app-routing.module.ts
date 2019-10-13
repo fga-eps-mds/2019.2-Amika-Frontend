8f7e426a089f02f1f9a47b2967a60479f80b1640
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-10-12 21:29:52 -0300

    Adicionando rota dos grupos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { TurmasComponent } from './turmas/turmas.component';
 import { TurmaEditComponent } from './turmas/turma-edit/turma-edit.component';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { GruposComponent } from './grupos/grupos.component';
 
 
 const routes: Routes = [
   { path: '',      component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   {path: 'turmas', component: TurmasComponent},
-  {path: 'turmas_editar/:id', component: TurmaEditComponent}
+  {path: 'turmas_editar/:id', component: TurmaEditComponent},
+  {path: 'grupos', component: GruposComponent}
 ];
 
 @NgModule({
