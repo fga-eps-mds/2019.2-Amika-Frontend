d770ec0a2814bca8098eeee5b891d531320fb11d
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-10-12 21:35:36 -0300

    Removendo objetos de teste

--- src/app/grupos/grupos.model.ts
+++ src/app/grupos/grupos.model.ts
@@ -3,22 +3,4 @@ export class Grupo {
     public nome: string;
 }
 
-export interface Grupo {
-    id: number; 
-    nome: string;
-  }
 
-  let grupo: Grupo[] = [
-    {
-      id: 56,
-      nome: 'Grupo 1'
-    },
-    {
-      id: 175,
-      nome: 'Grupo 2'
-    },
-    {
-        id: 12,
-        nome: 'Grupo 3'
-    }
-  ];
