db01229d4d7343abe90f968c59210c3da4563a66
Maicon Mares <maiconmaresunb@gmail.com>
2019-10-16 22:56:57 -0300

    Adicionando link do componente Agenda à Homepage.
    Co-authored-by: robertomatheus <matheus456456@gmail.com>
    Co-authored-by: Arthur Paiva <38506981+ArthurPaivaT@users.noreply.github.com>
    
    Signed-off-by: Maicon Mares <maiconmaresunb@gmail.com>

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateAgenda() {
+    this.router.navigate(['agenda']);
   }
 
 }
