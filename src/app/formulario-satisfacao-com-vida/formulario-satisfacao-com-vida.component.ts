63d3cd0f81ff818294cc8209edcfdb06d74bde38
Rafael Rodrigues <rafaelrodrigues@Rafaels-MacBook-Pro.local>
2019-10-22 11:43:04 -0300

    Alteração da página do formulário de satisfação com vida

--- src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.component.ts
+++ src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
-import {Pontuacao} from './pontos'
 
 @Component({
   selector: 'app-formulario-satisfacao-com-vida',
