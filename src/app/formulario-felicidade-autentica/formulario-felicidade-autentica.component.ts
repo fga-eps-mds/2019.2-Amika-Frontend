bed02bbddae3579e9cbe52cfefb2c5d3df15d135
Danilow200 <danilow200@gmail.com>
2019-10-25 17:46:19 -0300

    Mudança no formControlName

--- src/app/formulario-felicidade-autentica/formulario-felicidade-autentica.component.ts
+++ src/app/formulario-felicidade-autentica/formulario-felicidade-autentica.component.ts
@@ -21,9 +21,22 @@ export class FormularioFelicidadeAutenticaComponent implements OnInit {
 
   createForm(pontos: Pontuacao){
     this.formPontuacao = this.formBuilder.group({
+      ponto1: [pontos.pontos],
+      ponto2: [pontos.pontos],
+      ponto3: [pontos.pontos],
+      ponto4: [pontos.pontos],
+      ponto5: [pontos.pontos],
+      ponto6: [pontos.pontos],
+      ponto7: [pontos.pontos],
+      ponto8: [pontos.pontos],
+      ponto9: [pontos.pontos],
+      ponto10: [pontos.pontos],
+      ponto11: [pontos.pontos],
+      ponto12: [pontos.pontos],
       totalpontos: [pontos.pontos]
     })
   }
+  
 
   // ngOnInit() {
   // }
