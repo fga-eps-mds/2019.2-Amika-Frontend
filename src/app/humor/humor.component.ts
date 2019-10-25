f008dcb9afadd73bfe8a1dbca133a6ba545c7b9e
ArthurPaivaT <arthur001123@gmail.com>
2019-10-25 19:14:57 -0300

    Adicionando funcoes de modal e verificacao de erros do humor diario Co-authored-by: thefael <rafael-rodrigues-@live.com>

--- src/app/humor/humor.component.ts
+++ src/app/humor/humor.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, NgModule } from '@angular/core';
-import { RatingModule } from 'ngx-bootstrap/rating';
+import { Component, OnInit, NgModule, ViewChild } from '@angular/core';
+import { catchError } from 'rxjs/operators'
 import { Humor } from './humor.model';
 import { HumorService } from './humor.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Router, ActivatedRoute } from '@angular/router';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -14,12 +17,15 @@ export class HumorComponent implements OnInit {
 
   max = 5;
   rate = 0;
+  mensagem = "Humor Adicionado com sucesso";
   humor: Humor;
+  erroModalRef: BsModalRef;
+  @ViewChild('erroModal', {static: false}) erroModal;
 
   overStar: number | undefined;
   percent: number;
 
-  constructor(private humorService: HumorService) { }
+  constructor(private humorService: HumorService, private modalService: BsModalService, private router: Router) { }
 
   hoveringOver(value: number): void {
     this.overStar = value;
@@ -32,10 +38,25 @@ export class HumorComponent implements OnInit {
       aluno: 1,
       humor_do_dia: this.rate,
     });
-    this.humorService.create_humor(this.humor).subscribe();
+    this.humorService.create_humor(this.humor).subscribe(catchError( this.mostraErro() ));
     console.log(this.humor);
   }
 
+  mostraErro() : any{
+    console.log('UAI MANO')
+    this.mensagem = "Você já adicionou seu humor hoje!";
+    this.erroModalRef = this.modalService.show(this.erroModal, {class: 'modal-sm'});
+  }
+
+  homePage() {
+    this.router.navigate(['']);
+    console.log('adssddsasdd')
+  }
+
+  voltar(){
+    this.erroModalRef.hide();
+    this.mensagem= "Humor Adicionado com sucesso";
+  }
 
   ngOnInit() {
   }
