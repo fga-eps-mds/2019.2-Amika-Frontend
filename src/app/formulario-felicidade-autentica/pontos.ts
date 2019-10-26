dfb8ea00578a4e9078256b2f68aec9bd3f06dd47
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2019-10-26 10:29:17 -0300

    Alterando classe Pontuacao

--- src/app/formulario-felicidade-autentica/pontos.ts
+++ src/app/formulario-felicidade-autentica/pontos.ts
@@ -1,4 +1,14 @@
-export class Pontuacao{
-    pontos: number = 0;
-
+export class Pontuacao {
+    ponto1: number;
+    ponto2: number;
+    ponto3: number;
+    ponto4: number;
+    ponto5: number;
+    ponto6: number;
+    ponto7: number;
+    ponto8: number;
+    ponto9: number;
+    ponto10: number;
+    ponto11: number;
+    ponto12: number;
 }
