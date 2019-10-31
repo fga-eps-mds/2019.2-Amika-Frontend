19299790c0599521d5811a0bf192e62d88be6fee
matheus456 <matheus456456@gmail.com>
2019-10-31 00:15:03 -0200

    Adicionando sidebar e navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
 import { AutenticacaoService } from './autenticacao.service';
+import { 
+  Component, 
+  ChangeDetectorRef, 
+  EventEmitter, 
+  Output } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +13,27 @@ import { AutenticacaoService } from './autenticacao.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'amika-frontend';
-}
+  title = 'Amika';
+  mobileQuery: MediaQueryList;
+  isAdmin: boolean;
+  logado: boolean;
+  private _mobileQueryListener: () => void;
+  @Output() toggleSideNav = new EventEmitter();
+  
+  constructor( changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private autenticacao: AutenticacaoService ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
+
+  setTitle(nome: string) {
+    this.title = nome;
+  }
+
+  toggleMobileNav(nav: MatSidenav) {
+    if (this.mobileQuery.matches) {
+      nav.toggle();
+    }
+  }
+}
