89bfea9c10f22906139f8ffb952c04e33e4fe7f4
Danilow200 <danilow200@gmail.com>
2019-11-01 08:27:04 -0300

    Criando sistema de pontuação do formulario B

--- src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.component.ts
+++ src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from "@angular/router";
+import {Pontuacao} from './pontos'
+import { FormularioSatisfacaoComVidaService } from './formulario-satisfacao-com-vida.service';
 
 @Component({
   selector: 'app-formulario-satisfacao-com-vida',
@@ -9,9 +10,61 @@ import { Router } from "@angular/router";
 })
 export class FormularioSatisfacaoComVidaComponent implements OnInit {
 
-  constructor() { }
+  formPontuacao: FormGroup;
+  total: number = null;
+
+  //constructor() { }
+
+  constructor(private formBuilder: FormBuilder, private formularioSatisfacaoComVidaService: FormularioSatisfacaoComVidaService) { }
 
   ngOnInit() {
+    this.createForm(new Pontuacao());
   }
 
+  createForm(pontos: Pontuacao) {
+    this.formPontuacao = this.formBuilder.group({
+      ponto1: [pontos.ponto1, Validators.required],
+      ponto2: [pontos.ponto2, Validators.required],
+      ponto3: [pontos.ponto3, Validators.required],
+      ponto4: [pontos.ponto4, Validators.required],
+      ponto5: [pontos.ponto5, Validators.required],
+      ponto6: [pontos.ponto6, Validators.required],
+      ponto7: [pontos.ponto7, Validators.required],
+      ponto8: [pontos.ponto8, Validators.required],
+      ponto9: [pontos.ponto9, Validators.required],
+      ponto10: [pontos.ponto10, Validators.required],
+      ponto11: [pontos.ponto11, Validators.required],
+      ponto12: [pontos.ponto12, Validators.required],
+    })
+  }
+
+  fazerSoma(dadosFormulario){
+    let ponto = [];
+
+    ponto[1] = {pontos: dadosFormulario.ponto1};
+    ponto[2] = {pontos: dadosFormulario.ponto2};
+    ponto[3] = {pontos: dadosFormulario.ponto3};
+    ponto[4] = {pontos: dadosFormulario.ponto4};
+    ponto[5] = {pontos: dadosFormulario.ponto5};
+    ponto[6] = {pontos: dadosFormulario.ponto6};
+    ponto[7] = {pontos: dadosFormulario.ponto7};
+    ponto[8] = {pontos: dadosFormulario.ponto8};
+    ponto[9] = {pontos: dadosFormulario.ponto9};
+    ponto[10] = {pontos: dadosFormulario.ponto10};
+    ponto[11] = {pontos: dadosFormulario.ponto11};
+    ponto[12] = {pontos: dadosFormulario.ponto12};
+
+    this.total = ponto.reduce((total, valor) => total + valor.pontos, 0);
+
+    console.log(this.total);
+
+  }
+
+    onSubmit(dadosFormulario){
+      this.fazerSoma(dadosFormulario);
+      let totalpontos = {"formulario": [{"tipo": "B", "pontuacao": this.total}]}
+      this.formularioSatisfacaoComVidaService.enviar(totalpontos);
+      console.log(totalpontos);
+      this.formularioSatisfacaoComVidaService.errors;
+    }
 }
--- src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.service.ts
+++ src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.service.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 
-export class FormularioFelidadeAutenticaService {
+export class FormularioSatisfacaoComVidaService {
 
     errors;
     httpOptions = {
