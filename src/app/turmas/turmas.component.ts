5550f8ac8e6d1d5a41b9be21bdf53b457b14eeb2
matheus456 <matheus456456@gmail.com>
2019-11-02 01:06:20 -0200

    Adicionando dialog para adicionar turma

--- src/app/turmas/turmas.component.ts
+++ src/app/turmas/turmas.component.ts
@@ -1,9 +1,14 @@
 import { Turma } from './turmas.model';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { TurmaService } from './turma.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface CriarTurmasDialogoData {
+  formularioTurma: FormGroup;
+}
 
 @Component({
   selector: 'app-turmas',
@@ -22,7 +27,8 @@ export class TurmasComponent implements OnInit {
   turmaSelecionada: Turma;
 
   constructor(private turmaService: TurmaService, private formBuilder: FormBuilder,
-              private router: Router, private route: ActivatedRoute, private modalService: BsModalService) {
+              private router: Router, private route: ActivatedRoute, private modalService: BsModalService,
+              public dialog: MatDialog) {
     this.getter();
     this.formularioTurma = this.formBuilder.group({
       descricao: ['', Validators.required]
@@ -32,6 +38,18 @@ export class TurmasComponent implements OnInit {
   ngOnInit() {
   }
 
+  openDialog(): void {
+    this.formularioTurma.get('descricao').setValue("A");
+    const dialogRef = this.dialog.open(CriarTurmasDialogo, {
+      width: '250px',
+      data: {formularioTurma: this.formularioTurma}
+    });
+    dialogRef.afterClosed().subscribe(data => {
+      this.formularioTurma.value = JSON.parse(data)
+      this.onSubmit();
+    });
+  }
+  
   getter() {
     this.turmaService.get_turmas().subscribe((data: any) => {
       console.log(data);
@@ -84,6 +102,30 @@ export class TurmasComponent implements OnInit {
 
   onEdit(id) {
     this.router.navigate(['turmas_editar', id]);
-
   }
+}
+
+@Component({
+  selector: 'turmas-create-dialogo',
+  templateUrl: 'turmas-create-dialogo.html',
+  styleUrls: ['./turmas-create-dialogo.css']
+})
+export class CriarTurmasDialogo {
+
+  formularioTurma: FormGroup;
+
+  constructor( public dialogRef: MatDialogRef<CriarTurmasDialogo>, private formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: FormGroup ) {
+    this.formularioTurma = this.formBuilder.group({
+      descricao: ['', Validators.required]
+    });
+  }
+
+  onClick(): void {
+  }
+  
+  submit(form) {
+    console.log(form.value);
+    this.dialogRef.close(`${JSON.stringify(form.value)}`);
+  }
+
 }
