707eb9f4c41947302ad84627e9cb3739157623d3
thefael <rafael-rodrigues-@live.com>
2019-11-09 14:54:17 -0300

    Criação do método de upload de arquivos
    Co-authored-by: danilow200 <danilow200@gmail.com>

--- src/app/materiais/materiais.component.ts
+++ src/app/materiais/materiais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-materiais',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MateriaisComponent implements OnInit {
 
-  constructor() { }
+  SERVER_URL = "http://localhost:4200/upload"
+  uploadForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      profile: ['']
+    });
   }
 
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('profile').setValue(file);
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.uploadForm.get('profile').value);
+
+    this.httpClient.post<any>(this.SERVER_URL, formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+
+  }
 }
