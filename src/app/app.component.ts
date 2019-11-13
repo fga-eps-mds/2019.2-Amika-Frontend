8ab8ac74c6a1ddc39446ff98fb4d64cd3b06b41c
Vinícius Cantuária <cantuariavc@gmail.com>
2019-11-13 18:13:34 -0300

    Adiciona troca de foto de perfil de aluno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,13 @@ import {
   ChangeDetectorRef,
   EventEmitter,
   OnInit,
+  OnDestroy,
   Output } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material';
+import { PerfilService } from './perfil.service';
+import { environment } from '../environments/environment';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,19 +18,40 @@ import { MatSidenav } from '@angular/material';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Amika';
   mobileQuery: MediaQueryList;
   isAdmin: boolean;
   logado: boolean;
-  private _mobileQueryListener: () => void;
   @Output() toggleSideNav = new EventEmitter();
 
-  constructor( changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public autenticacao: AutenticacaoService ) {
+  username: string;
+  firstName: string;
+  grupo: string;
+  foto: string;
+
+  private readonly _mobileQueryListener: () => void;
+  private subscriptions: Subscription = new Subscription();
+  private userId: string = localStorage.getItem('user_id');
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    public autenticacao: AutenticacaoService,
+    private perfilService: PerfilService,
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit() {
     this.isSuperuser();
+    this.getPerfil();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   setTitle(nome: string) {
@@ -37,12 +62,10 @@ export class AppComponent {
     const params = this.autenticacao.getJWTParams();
     if (!params) {
       this.logado = false;
-    }
-    else if (params['superusuario']) {
+    } else if (params.superusuario) {
       this.isAdmin = true;
       this.logado = true;
-    }
-    else {
+    } else {
       this.isAdmin = false;
       this.logado = true;
     }
@@ -52,5 +75,39 @@ export class AppComponent {
     if (this.mobileQuery.matches) {
       nav.toggle();
     }
+  }
+
+  getPerfil() {
+    this.subscriptions.add(this.perfilService.perfilAluno(this.userId)
+      .subscribe((data: any) => {
+        this.username = data.username;
+        this.firstName = data.first_name;
+        this.grupo = data.grupo;
+        if (data.foto) {
+          this.foto = `${environment.urlApi}${data.foto.slice(1)}`;
+        } else {
+          this.foto = 'https://cf.ltkcdn.net/socialnetworking/images/orig/168646-425x425-Cat-Avatar-Primary.jpg';
+        }
+      }));
+  }
+
+  adicionaFoto() {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+    input.addEventListener('input', (e: Event) => {
+      this.trocaFoto((e.target as HTMLInputElement).files[0]);
+    });
+  }
+
+  trocaFoto(foto) {
+    const formData = new FormData();
+    formData.append('foto', foto);
+
+    this.subscriptions.add(this.perfilService.trocaFoto(this.userId, formData)
+      .subscribe(() => {
+        this.getPerfil();
+      }));
   }
 }
