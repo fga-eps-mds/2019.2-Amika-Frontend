7754677df6b8731aee2f1cffd8f857dd22d0e8a6
ArthurPaivaT <arthur001123@gmail.com>
2019-11-15 09:09:18 -0300

    Alterando modo que o form pega o anexo
    Co-authored-by: thefael <rafael-rodrigues-@live.com>

--- src/app/materiais/materiais.component.ts
+++ src/app/materiais/materiais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { MateriaisService } from './materiais.service';
 
 @Component({
   selector: 'app-materiais',
@@ -11,30 +12,65 @@ export class MateriaisComponent implements OnInit {
 
   SERVER_URL = "http://localhost:4200/upload"
   uploadForm: FormGroup;
+  anexo;
+  error: any = {isError: false, errorMessage: ''};
 
-  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
+  constructor(private materiaisService: MateriaisService, private formBuilder: FormBuilder, private httpClient: HttpClient) {
 
-  ngOnInit() {
     this.uploadForm = this.formBuilder.group({
-      profile: ['']
+      arquivo: ['']
     });
+
   }
 
-  onFileSelect(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.uploadForm.get('profile').setValue(file);
+  ngOnInit() {
+  }
+
+
+  onChange(event){
+    if (event.target.files.length > 0){
+      const file =  event.target.files[0];
+      this.anexo = file;
+
+      this.uploadForm.controls['arquivo'].setValue(file);
+      console.log(this.uploadForm)
     }
   }
 
-  onSubmit() {
+  onSubmit(){
     const formData = new FormData();
-    formData.append('file', this.uploadForm.get('profile').value);
 
-    this.httpClient.post<any>(this.SERVER_URL, formData).subscribe(
-      (res) => console.log(res),
-      (err) => console.log(err)
-    );
+    if (this.uploadForm.get('arquivo').value === null || this.uploadForm.get('arquivo').value === "") {
+      console.log("Não há arquivos para enviar");
+      this.uploadForm.controls['arquivo'].setValue(null);
+    } else {
+      formData.append('arquivo', this.uploadForm.get('arquivo').value);
+    }
+
+    if (this.uploadForm.valid) {
+      this.materiaisService.create_materiais(formData).subscribe(
+        (data: any) => {
+          this.uploadForm.reset();
+        },
+        (error: any) => {
+          this.error = error;
+          console.log(error);
+        }
+        )
+      }
 
   }
+
+  // onSubmit() {
+  //   // console.log(this.uploadForm.value);
+  //   const formData = new FormData();
+  //   formData.append('material', this.uploadForm.get('arquivo').value);
+
+  //   console.log(formData);
+  //   this.httpClient.post<any>(this.SERVER_URL, formData).subscribe(
+  //   (res) => console.log(res),
+  //   (err) => console.log(err)
+  // );
+  // }
+
 }
