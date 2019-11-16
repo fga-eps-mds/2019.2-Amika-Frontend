716c9ad16f56305bf58794a72646d67abcd30bf5
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-11-16 12:02:25 -0300

    Estilizando componente

--- src/app/grupo/grupo.component.ts
+++ src/app/grupo/grupo.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grupo.component.css']
 })
 export class GrupoComponent implements OnInit {
-
+  nome : string;
+  alunos : any;
   constructor() { }
 
   ngOnInit() {
--- src/app/grupo/lista-grupo.service.ts
+++ src/app/grupo/lista-grupo.service.ts
@@ -1,9 +1,16 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AutenticacaoService } from './../autenticacao.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaGrupoService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient, private autenticacao: AutenticacaoService) { }
+
+  public get_grupo(): Observable<any> {
+    return this.http.get(environment.urlApi + 'grupo/', this.autenticacao.httpOptions);
+  }
