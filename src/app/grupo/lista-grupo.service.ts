7863a6c805ed627fdedd4eb6b15810de866c4c29
Ithalo Azevedo <ithaloazvd@gmail.com>
2019-11-19 16:50:20 -0300

    Setando headers e linkando o componente com a sidebar
    Co-authored-by: ArthurPaivaT <arthur001123@gmail.com>

--- src/app/grupo/lista-grupo.service.ts
+++ src/app/grupo/lista-grupo.service.ts
@@ -9,9 +9,11 @@ import { environment } from 'src/environments/environment';
 })
 export class ListaGrupoService {
 
-  constructor(private http: HttpClient, private autenticacao: AutenticacaoService) { }
+  constructor(private http: HttpClient, private autenticacao: AutenticacaoService) { 
+    this.autenticacao.setHeader();
+  }
 
   public get_grupo(): Observable<any> {
     return this.http.get(environment.urlApi + 'alunos_grupo/', this.autenticacao.httpOptions);
   }
-}
+}''
