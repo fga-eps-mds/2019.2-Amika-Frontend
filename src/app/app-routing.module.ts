999ae5dd6ae9192e0e6275fec6393ce929353f5b
Vinícius Cantuária <cantuariavc@gmail.com>
2019-11-23 09:30:38 -0300

    Corrige link em agendas na barra lateral

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
   { path: 'turmas_editar/:id', component: TurmaEditComponent, canActivate: [AutenticacaoService] },
   { path: 'agenda', component: AgendasComponent, canActivate: [AutenticacaoService] },
   { path: 'agenda-edit/:id', component: AgendaEditComponent, canActivate: [AutenticacaoService]},
-  { path: 'agendas-realizadas', component: AgendasRealizadasComponent},
+  { path: 'agendas-realizadas', component: AgendasRealizadasComponent },
   { path: 'area-informativa', component: AreaInformativaComponent},
   { path: 'grafico/:id', component : GraficoHumorComponent},
   { path: 'cadastrar', component: AlunoAutoRegistroComponent, canActivate: [AutenticacaoService] },
