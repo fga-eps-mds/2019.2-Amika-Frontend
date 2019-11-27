9c91c29927910a8f97e5d6150929043be83a3f3c
Maicon Mares <maiconmaresunb@gmail.com>
2019-11-26 21:06:36 -0300

    Refatorando formularios.

--- src/app/formulario-felicidade-autentica/formulario_felicidade_autentica.service.ts
+++ src/app/formulario-felicidade-autentica/formulario_felicidade_autentica.service.ts
@@ -1,8 +1,8 @@
 import { AutenticacaoService } from './../autenticacao.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +10,28 @@ import { environment } from 'src/environments/environment';
 
 export class FormularioFelidadeAutenticaService {
 
-    constructor(private http: HttpClient, private autenticacao: AutenticacaoService) {
-        this.autenticacao.setHeader();
-     }
+  errors;
+  httpOptions = {
+      headers: new HttpHeaders({
+          'Content-Type':  'application/json'
+      })
+  };
 
-    enviar(dados): Observable<any> {
-        console.log("this.autenticacao.httpOptions");
-        console.log(this.autenticacao.httpOptions);
-        console.log(dados);
-        console.log(this.autenticacao.httpOptions.headers);
-        console.log("this.autenticacao.httpOptions");
-        return this.http.put(environment.urlApi + "aluno/" + localStorage.getItem('user_id') + "/", dados, this.autenticacao.httpOptions);
-    }
+    constructor(private http: HttpClient, private autenticacao: AutenticacaoService) { }
+
+    enviar(dados) {
+        return this.http.put(environment.urlApi + "aluno/" + localStorage.getItem('user_id') + "/", dados, this.autenticacao.httpOptions)
+                .subscribe(data => {
+                  console.log(data);
+
+              },
+              error => {
+                  console.log(error);
+                  this.errors = error;
+              });
+            }
 
     public verificaForm() {
-      return this.http.get("http://localhost:8000/aluno/" + localStorage.getItem('user_id') + "/", this.autenticacao.httpOptions);
+      return this.http.get(environment.urlApi + "aluno/" + localStorage.getItem('user_id') + "/", this.autenticacao.httpOptions);
     }
 }
--- src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.service.ts
+++ src/app/formulario-satisfacao-com-vida/formulario-satisfacao-com-vida.service.ts
@@ -2,24 +2,19 @@ import { AutenticacaoService } from './../autenticacao.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class FormularioSatisfacaoComVidaService {
-
     errors;
-    httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type':  'application/json'
-        })
-    };
 
     constructor(private http: HttpClient, private router: Router, private autenticacao: AutenticacaoService) { }
 
     enviar(dados) {
-        return this.http.put("http://localhost:8000/aluno/" + localStorage.getItem('user_id') + "/", dados, this.autenticacao.httpOptions)
+        return this.http.put(environment.urlApi + "aluno/" + localStorage.getItem('user_id') + "/", dados, this.autenticacao.httpOptions)
                 .subscribe(data => {
                     console.log(data);
 
@@ -31,6 +26,6 @@ export class FormularioSatisfacaoComVidaService {
     }
 
     public verificaForm() {
-      return this.http.get("http://localhost:8000/aluno/" + localStorage.getItem('user_id') + "/", this.autenticacao.httpOptions);
+      return this.http.get(environment.urlApi + "aluno/" + localStorage.getItem('user_id') + "/", this.autenticacao.httpOptions);
     }
 }
