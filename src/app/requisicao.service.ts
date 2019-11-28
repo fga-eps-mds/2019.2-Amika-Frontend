aea20b5278a2b34a2de3f3dda9e5fe898636b286
MateusO97 <mat97_@hotmail.com>
2019-11-27 23:10:59 -0200

    Adicionando mensagem de validacao ao registrar

--- src/app/requisicao.service.ts
+++ src/app/requisicao.service.ts
@@ -1,5 +1,6 @@
 import { RequisicoesService } from './requisicoes.service';
 import { Injectable } from '@angular/core';
+import { AlertaService } from './alerta.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,18 +8,19 @@ import { Injectable } from '@angular/core';
 export class RequisicaoService {
   errors;
 
-  constructor(private requisicao: RequisicoesService) {}
+  constructor(private requisicao: RequisicoesService, public alertaService:AlertaService) {}
 
   realizarRequisicao(dadosAluno) {
     return this.requisicao.post('aluno/', dadosAluno)
                     .subscribe(data => {
                       console.log(data);
                       this.errors = null;
+                      this.alertaService.alerta('Seu cadastro foi feito com sucesso!', 'success', false);
                     },
                     error => {
                       console.log(error);
                       this.errors = error;
-
+                      this.alertaService.alerta('Sua matricula ainda não foi cadastrada, solicite ao professor para que realize o cadastro de sua matricula anteriormente. ', 'error', false);
                     });
   }
 
